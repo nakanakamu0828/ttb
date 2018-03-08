456b582546d1184313134593300039449971b55b
nakanakamu0828 <yuuki.nakamura.0828@gmail.com>
2018-03-09 02:47:10 +0900

    refact: changed mobile footer layout.

--- assets/main.js
+++ assets/main.js
@@ -1,4 +1,8 @@
 
+function getAll(selector) {
+    return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     // Get all "navbar-burger" elements
     var $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
@@ -23,15 +27,57 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     var elem = document.querySelector('.main-carousel');
-    var flkty = new Flickity( elem, {
-    // options
-    cellAlign: 'left',
-    contain: true
+    if (elem) {
+        var flkty = new Flickity( elem, {
+            // options
+            cellAlign: 'left',
+            contain: true
+        });
+    }
+
+    // To Top
+    var tap = null === window.ontouchstart ? "touchstart" : "click";
+    var elem = document.getElementById("linkTotop");
+    elem.addEventListener(tap, function(){
+        document.body.scrollTop = document.documentElement.scrollTop = 0;
+    }, false);
+
+    // Modals
+    var rootEl = document.documentElement;
+    var $modals = getAll('.modal');
+    var $modalButtons = getAll('.modal-button');
+    var $modalCloses = getAll('.modal-background, .modal-close, .modal-card-head .delete, .modal-card-foot .button');
+    if ($modalButtons.length > 0) {
+    $modalButtons.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+        var target = $el.dataset.target;
+        var $target = document.getElementById(target);
+        rootEl.classList.add('is-clipped');
+        $target.classList.add('is-active');
+        });
+    });
+    }
+
+    if ($modalCloses.length > 0) {
+    $modalCloses.forEach(function ($el) {
+        $el.addEventListener('click', function () {
+        closeModals();
+        });
+    });
+    }
+
+    document.addEventListener('keydown', function (event) {
+    var e = event || window.event;
+    if (e.keyCode === 27) {
+        closeModals();
+        closeDropdowns();
+    }
     });
 
-    // element argument can be a selector string
-    //   for an individual element
-    var flkty = new Flickity( '.main-carousel', {
-    // options
+    function closeModals() {
+    rootEl.classList.remove('is-clipped');
+    $modals.forEach(function ($el) {
+        $el.classList.remove('is-active');
     });
+    }
 });
